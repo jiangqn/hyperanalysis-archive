06fd953c4ffd05dabe41c2a2fa7e8f9ee4250c61
jiangqn <1935448858@qq.com>
2020-11-28 23:06:04 +0800

    add positive_define_matrix_power

--- hyperanalysis/utils/linalg.py
+++ hyperanalysis/utils/linalg.py
@@ -1,5 +1,7 @@
 import torch
 
+eps = 1e-6
+
 def cov(X: torch.FloatTensor, Y: torch.FloatTensor = None) -> torch.FloatTensor:
     """
     :param X: (num, x_dim)
@@ -16,7 +18,17 @@ def cov(X: torch.FloatTensor, Y: torch.FloatTensor = None) -> torch.FloatTensor:
 
     X_mean = X.mean(dim=0, keepdim=True)
     Y_mean = Y.mean(dim=0, keepdim=True)
-    X = X - X_mean
-    Y = Y - Y_mean
+    X -= X_mean
+    Y -= Y_mean
     C = X.t().matmul(Y) / (num - 1)
     return C
+
+def check_matrix_symmetric(X: torch.FloatTensor) -> None:
+    assert len(X.size()) == 2
+    assert X.size(0) == X.size(1)
+    assert (X - X.t()).abs().max().item() <= eps
+
+def postive_define_matrix_power(X: torch.FloatTensor, power=1) -> torch.FloatTensor:
+    check_matrix_symmetric(X)
+    U, S, V = torch.svd(X)
+    return U.matmul(torch.diag(torch.pow(S, power))).matmul(V.t())
