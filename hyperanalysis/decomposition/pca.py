930126286f28deee304c5370a10042b960f41c0f
jiangqn <1935448858@qq.com>
2020-11-28 21:42:39 +0800

    add n_components type and range check

--- hyperanalysis/decomposition/pca.py
+++ hyperanalysis/decomposition/pca.py
@@ -2,9 +2,12 @@ import torch
 
 class PCA(object):
 
-    def __init__(self, n_components=None):
+    def __init__(self, n_components=None, explained_variance_ratio=None) -> None:
         super(PCA, self).__init__()
+        assert n_components == None or explained_variance_ratio == None
+        assert explained_variance_ratio == None or (explained_variance_ratio >= 0 and explained_variance_ratio <= 1)
         self.n_components = n_components
+        self._explained_variance_ratio = explained_variance_ratio
 
     def fit(self, X: torch.FloatTensor) -> None:
         self._fit(X)
@@ -21,8 +24,8 @@ class PCA(object):
         assert len(X.size()) == 2
         num, dim = X.size()
 
-        if self.n_components == None:
-            self.n_components = min(num, dim)
+        self.mean_ = X.mean(dim=0, keepdim=True)
+        X -= self.mean_
 
         _, S, V = torch.svd(X)
 
@@ -30,11 +33,26 @@ class PCA(object):
         S = S.index_select(index=index, dim=0)
         V = V.index_select(index=index, dim=1)
 
-        eigenvalues = S * S / (num - 1)
-        self.explained_variance_ = eigenvalues[0:self.n_components].tolist()
-        self.explained_variance_ratio_ = (eigenvalues[0:self.n_components] / eigenvalues.sum()).tolist()
+        eigen_values = S * S / (num - 1)
+
+        if self.n_components == None:
+            if self._explained_variance_ratio == None:
+                self.n_components = min(num, dim)
+            else:
+                for n_components in range(1, min(num, dim) + 1):
+                    explained_variance_ratio = (eigen_values[0:n_components].sum() / eigen_values.sum()).item()
+                    if explained_variance_ratio >= self._explained_variance_ratio:
+                        self.n_components = n_components
+                        break
+
+        assert isinstance(self.n_components, int)
+        assert self.n_components >= 1 and self.n_components <= min(num, dim)
+
+        self.explained_variance_ = eigen_values[0:self.n_components].tolist()
+        self.explained_variance_ratio_ = (eigen_values[0:self.n_components] / eigen_values.sum()).tolist()
         self.V_ = V[:, 0:self.n_components]
 
     def _transform(self, X: torch.FloatTensor) -> torch.FloatTensor:
+        assert hasattr(self, "mean_")
         assert hasattr(self, "V_")
-        return X.matmul(self.V_)
+        return (X - self.mean_).matmul(self.V_)
