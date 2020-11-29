ca135a0193b0670b1577b41d99b1503fbc7bf695
jiangqn <1935448858@qq.com>
2020-11-29 10:33:42 +0800

    update cca

--- hyperanalysis/cross_decomposition/cca.py
+++ hyperanalysis/cross_decomposition/cca.py
@@ -20,6 +20,8 @@ class CCA(object):
 
     def _fit(self, X: torch.FloatTensor, Y: torch.FloatTensor) -> None:
 
+        X, Y = X.clone(), Y.clone()
+
         assert len(X.size()) == 2
         assert len(Y.size()) == 2
         assert X.size(0) == Y.size(0)
@@ -36,8 +38,15 @@ class CCA(object):
 
         self.X_mean_ = X.mean(dim=0, keepdim=True)
         self.Y_mean_ = Y.mean(dim=0, keepdim=True)
-        X -= self.X_mean_
-        Y -= self.Y_mean_
+
+        X_std_ = X.std(dim=0, keepdim=True)
+        X_std_[X_std_ == 0.0] = 1.0
+        Y_std_ = Y.std(dim=0, keepdim=True)
+        Y_std_[Y_std_ == 0.0] = 1.0
+        self.X_std_, self.Y_std_ = X_std_, Y_std_
+
+        X = (X - self.X_mean_) / self.X_std_
+        Y = (Y - self.Y_mean_) / self.Y_std_
 
         SXX = cov(X, X)
         SXY = cov(X, Y)
@@ -52,8 +61,18 @@ class CCA(object):
         self.corr_coefs_ = S[0:self.n_components_].tolist()
         self.A_ = PSXX.matmul(U[:, 0:self.n_components_])
         self.B_ = PSYY.matmul(V[:, 0:self.n_components_])
+        self.A_ = self.A_ / torch.norm(self.A_, dim=0, keepdim=True)
+        self.B_ = self.B_ / torch.norm(self.B_, dim=0, keepdim=True)
+
 
     def _transform(self, X: torch.FloatTensor, Y: torch.FloatTensor) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
-        X = (X - self.X_mean_).matmul(self.A_)
-        Y = (Y - self.Y_mean_).matmul(self.B_)
+
+        X, Y = X.clone(), Y.clone()
+
+        X = (X - self.X_mean_) / self.X_std_
+        Y = (Y - self.Y_mean_) / self.Y_std_
+
+        X = X.matmul(self.A_)
+        Y = Y.matmul(self.B_)
+
         return X, Y
--- tests/test_cca.py
+++ tests/test_cca.py
@@ -3,6 +3,8 @@ sys.path.append("..")
 import torch
 import numpy as np
 from hyperanalysis.cross_decomposition.cca import CCA
+from sklearn.cross_decomposition.cca_ import CCA as sCCA
+from scipy.stats import pearsonr
 
 path = "../embedding.npy"
 
@@ -12,6 +14,26 @@ X = torch.from_numpy(X).float()
 Y = X[:, 100:250]
 X = X[:, 0:100]
 
+scca = sCCA(n_components=2, scale=True)
+sx, sy = scca.fit_transform(X, Y)
+
 cca = CCA(n_components=2)
-cca.fit(X, Y)
-print(cca.corr_)
+x, y = cca.fit_transform(X, Y)
+x, y = x.numpy(), y.numpy()
+
+print(pearsonr(sx[:, 0], sy[:, 0]))
+print(pearsonr(sx[:, 1], sy[:, 1]))
+
+print(pearsonr(x[:, 0], y[:, 0]))
+print(pearsonr(x[:, 1], y[:, 1]))
+print(cca.corr_coefs_)
+
+print("sx mean:", sx.mean(axis=0))
+print("x mean:", x.mean(axis=0))
+print("sx std:", sx.std(axis=0, ddof=1))
+print("x std:", x.std(axis=0, ddof=1))
+
+print(sx)
+print(x)
+print(sy)
+print(y)
