0c1804df43b69598d1a1532d3f518d7a065dcfd0
jiangqn <1935448858@qq.com>
2020-11-29 10:42:06 +0800

    Update test_truncated_svd.py

--- tests/test_truncated_svd.py
+++ tests/test_truncated_svd.py
@@ -3,12 +3,16 @@ import numpy as np
 import sys
 sys.path.append("..")
 from hyperanalysis.decomposition.truncated_svd import TruncatedSVD
+from sklearn.decomposition.truncated_svd import TruncatedSVD as sTruncatedSVD
 
 path = "../embedding.npy"
 
 X = np.load(path)
+
+stsvd = sTruncatedSVD(n_components=2)
+sX = stsvd.fit_transform(X)
+
 X = torch.from_numpy(X).float()
 
 tsvd = TruncatedSVD(n_components=2)
-X = tsvd.fit_transform(X)
-print(X.shape)
+X = tsvd.fit_transform(X).numpy()
