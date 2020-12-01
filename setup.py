b3ea3a368147932dbdb9bf6b2b54c0d7accded3b
jiangqn <1935448858@qq.com>
2020-12-01 22:13:44 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperanalysis",
+    version="0.1",
+    author="jiangqn",
+    author_email="jiangqn.nju@gmail.com",
+    description="A GPU-accelerated data analysis toolkit.",
+    url="https://github.com/jiangqn/hyperanalysis",
+    packages=find_packages()
+)
