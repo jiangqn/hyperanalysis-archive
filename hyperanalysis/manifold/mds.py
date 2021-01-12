3505675272d5f8ba56c926b6d4d073193b55c36b
jiangqn <1935448858@qq.com>
2021-01-13 01:54:40 +0800

    Update mds.py

--- hyperanalysis/manifold/mds.py
+++ hyperanalysis/manifold/mds.py
@@ -2,20 +2,21 @@ import torch
 
 class MDS(object):
 
-    def __init__(self, n_components=None):
+    def __init__(self, n_components=None) -> None:
         super(MDS, self).__init__()
+        self.n_components = n_components
 
-    def fit(self):
+    def fit(self, X: torch.FloatTensor) -> None:
         pass
 
-    def fit_transform(self):
+    def fit_transform(self, X: torch.FloatTensor) -> torch.FloatTensor:
         pass
 
-    def transform(self):
+    def transform(self, X: torch.FloatTensor) -> torch.FloatTensor:
         pass
 
-    def _fit(self):
+    def _fit(self, X: torch.FloatTensor) -> None:
         pass
 
-    def _transform(self):
+    def _transform(self, X: torch.FloatTensor) -> torch.FloatTensor:
         pass
